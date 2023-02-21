085d758cf27ce91f7c851773598ac3f42cd72046
hopeVaughn <hopevaughnwarren@gmail.com>
2023-02-21 15:40:06 -0800

    add tailwind. Update tailwind config

---
+++ app/about/loading.jsx
@@ -0,0 +1,8 @@
+export default function Loading(){
+  // you can add any UI inside Loading, including skeleton
+  return(
+    <div>
+      <h1>Loading...</h1>
+    </div>
+  )
+}
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,11 @@
 import './globals.css'
+import { Montserrat } from "@next/font/google"
+
+const montserrat = Montserrat({
+  weight: ['400', '700'],
+  subsets: ["latin"],
+  variable: '--font-montserrat'
+});
 
 export default function RootLayout({ children }) {
   return (
@@ -9,7 +16,7 @@ export default function RootLayout({ children }) {
       */}
       <head />
 
-      <body>
+      <body className={`${montserrat.className}`}>
         <nav>
           <h1>logo</h1>
           <ul>

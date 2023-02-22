bedc7eb5bea98f32c5858369f59da6a80dd00411
hopeVaughn <hopevaughnwarren@gmail.com>
2023-02-21 16:52:08 -0800

    add pre render function to page in movies dir

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,9 +1,15 @@
 import Image from "next/image"
-
+export async function generatStaticParams() {
+  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+  const res = await data.json()
+  return res.results.map((movie)=>({
+    movie: toString(movie.id),
+  }))
+}
 export default async function MovieDetail({ params }) {
   const { movie } = params
   const imagePath = 'https://image.tmdb.org/t/p/original'
-  const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`, {next: {revalidate: 10}})
+  const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`)
   const res = await data.json()
   return (
     <div className="">
@@ -12,12 +18,12 @@ export default async function MovieDetail({ params }) {
         <h2 className="text-lg">{res.release_date}</h2>
         <h2>Runtime: {res.runtime} minutes</h2>
         <h2 className="bg-green-600 inline-block my-2 py-2 px-4 rounded-lg text-sm">{res.status}</h2>
-        <Image 
-        className="my-12 w-full" 
-        src={imagePath + res.backdrop_path}
-        width={1000}
-        height={1000}
-        priority
+        <Image
+          className="my-12 w-full"
+          src={imagePath + res.backdrop_path}
+          width={1000}
+          height={1000}
+          priority
         />
         <p>{res.overview}</p>
       </div>
